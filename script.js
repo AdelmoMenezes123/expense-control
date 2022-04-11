792ef5138c99ad8b2fae3131d393174ba6418de1
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2022-04-10 21:51:23 -0300

    finaliza crud grava no localStorage

--- script.js
+++ script.js
@@ -1,53 +1,107 @@
+const transactionsUl = document.querySelector('#transactions');
+const icomeDisplay = document.querySelector('#money-plus');
+const expanseDisplay = document.querySelector('#money-minus');
+const balancelDisplay = document.querySelector('#balance');
+const form = document.querySelector('#form');
+const inputTransactionName = document.querySelector('#text');
+const inputTransactionAmount = document.querySelector('#amount');
 
+const localStorageTrasactions =
+	JSON.parse(localStorage.getItem('transactions'));
 
-export class Backend {	
+let transactions =
+	localStorage
+		.getItem('transactions') !== null
+		? localStorageTrasactions
+		: [];
 
-	constructor() {
-		this.transactionsUl = document.querySelector('#transactions');
-		this.icomeDisplay = document.querySelector('#money-plus');
-		this.expanseDisplay = document.querySelector('#money-minus');
-		this.balancelDisplay = document.querySelector('#balance');
-	
-		this.dummyTransactions = [
-			{ id: 1, name: 'Bolo de brigadeiro', amount: -20 },
-			{ id: 2, name: 'Salario', amount: 300 },
-			{ id: 3, name: 'Torta de frango', amount: -10 },
-			{ id: 4, name: 'Violao', amount: 150 },
-		];
-	}
-
-	init() {
-		const me = this;
-		this.dummyTransactions.forEach(me.addTransactionIntoDOM)
-		me.updateBalanceValue(this.dummyTransactions)
-	}
-
-	updateBalanceValue = (dummyTransactions) => {
-		const me = this;
-		const transactionsAmount = dummyTransactions.map(transaction => transaction.amount);
-		const total = transactionsAmount.reduce((acc, transaction) => acc + transaction, 0).toFixed(2);
-		const income = transactionsAmount.filter(value=> value > 0).reduce((acc, value) => acc + value, 0).toFixed(2);
-		const expanse =Math.abs( transactionsAmount.filter(value => value < 0).reduce((acc, value) => acc + value, 0)).toFixed(2);
-	
-		me.balancelDisplay.textContent = `R$ ${total}`
-		me.icomeDisplay.textContent = `R$ ${income}`
-		me.expanseDisplay.textContent = `R$ ${expanse}`
-	}
-
-	addTransactionIntoDOM = ({ amount, name, id }) => {
-		const operation = amount < 0 ? '-' : '+'
-		const CSSClass = amount < 0 ? 'minus' : 'plus'
-		const amountWithoutOperation = Math.abs(amount)
-		const li = document.createElement('li');
-
-		li.classList.add(CSSClass);
-		li.innerHTML = `
-		${name} <span>${operation} R$ ${amountWithoutOperation}</span><button class="delete-btn">x</button>	
-	`
-
-		this.transactionsUl.prepend(li)
-	}
+const removeTransaction = ID => {
+	transactions =
+		transactions
+			.filter(({ id }) => id !== ID);
+	updateLocalStorage();
+	init();
 }
 
-let backend = new Backend();
-backend.init()
+const addTransactionIntoDOM = ({ amount, name, id }) => {
+	const operation = amount < 0 ? '-' : '+'
+	const CSSClass = amount < 0 ? 'minus' : 'plus'
+	const amountWithoutOperation = Math.abs(amount)
+	const li = document.createElement('li');
+
+	li.classList.add(CSSClass);
+	li.innerHTML = `
+	${name} <span>${operation} R$ ${amountWithoutOperation}</span>
+	<button class="delete-btn" onClick="removeTransaction(${id})">x</button>	
+`
+
+		transactionsUl.prepend(li)
+}
+
+const getExpense = transactionsAmount => Math.abs(transactionsAmount
+	.filter(value => value < 0)
+	.reduce((acc, value) => acc + value, 0))
+	.toFixed(2);
+ 
+
+const getTotal = transactionsAmount => transactionsAmount
+	.reduce((acc, transaction) => acc + transaction, 0)
+	.toFixed(2); 
+
+const getIncome = transactionsAmount => transactionsAmount
+	.filter(value => value > 0)
+	.reduce((acc, value) => acc + value, 0)
+	.toFixed(2); 
+
+const updateBalanceValue = () => {
+	const transactionsAmount = transactions.map(({ amount }) => amount);
+	const total = getTotal(transactionsAmount)
+	const income = getIncome(transactionsAmount)
+	const expanse = getExpense(transactionsAmount)
+
+	balancelDisplay.textContent = `R$ ${total}`
+	icomeDisplay.textContent = `R$ ${income}`
+	expanseDisplay.textContent = `R$ ${expanse}`
+}
+
+const init = () => {
+	transactionsUl.innerHTML = '';
+	transactions.forEach(addTransactionIntoDOM)
+	updateBalanceValue()
+}
+
+init();
+
+const updateLocalStorage = () => {
+	localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+const generateId = () => Math.round(Math.random() * 1000);
+
+const addToTransactionArray = (transactionName, transactionAmount) => {
+	transactions.push({
+		id: generateId(),
+		name: transactionName,
+		amount: Number(transactionAmount),
+	});
+}
+
+const handleFormSubmit = event => {
+	event.preventDefault();
+
+	const transactionName = inputTransactionName.value.trim();
+	const transactionAmount = inputTransactionAmount.value.trim();
+	const isSomeInputEmpty = transactionAmount === '' || transactionName === '';
+
+	if (isSomeInputEmpty) {
+		alert('Preencha tanto o nome quanto o valor da transação');
+		return
+	}
+
+	addToTransactionArray(transactionName, transactionAmount)
+	init();
+	updateLocalStorage();
+	form.reset()
+ }
+
+form.addEventListener('submit', handleFormSubmit);
