c9d051aa41bfc648e1fe3eea3450ee3c8f3f4a6a
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2022-04-10 23:04:34 -0300

    muda nome de arquivo json

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('install', function(event){
             cache.addAll([
                 './',
                 './index.html',
-                './manifest.json',
+                './manifest.webmanifest',
 								'./script.js',
 								'/.style.css',
 								'/.favicon.png'
