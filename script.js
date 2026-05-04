045ca7b59642dc956ee6080dd53cce0f61c629be
Adelmo <adelmo@connectlab.com>
2026-05-04 13:42:03 -0300

    v1: dark mode

--- script.js
+++ script.js
@@ -186,15 +186,24 @@ btnHistory.addEventListener("click", () => {
   }
 
   const monthNames = {
-    "01": "Janeiro", "02": "Fevereiro", "03": "Março", "04": "Abril",
-    "05": "Maio", "06": "Junho", "07": "Julho", "08": "Agosto",
-    "09": "Setembro", "10": "Outubro", "11": "Novembro", "12": "Dezembro"
+    "01": "Janeiro",
+    "02": "Fevereiro",
+    "03": "Março",
+    "04": "Abril",
+    "05": "Maio",
+    "06": "Junho",
+    "07": "Julho",
+    "08": "Agosto",
+    "09": "Setembro",
+    10: "Outubro",
+    11: "Novembro",
+    12: "Dezembro",
   };
 
   modalTitle.textContent = `Relatório de ${monthNames[selectedMonth]}`;
 
   modalTransactionsUl.innerHTML = "";
-  
+
   // Filtra as transações apenas pelo mês (ignora o ano)
   const pastTransactions = transactions.filter((t) => {
     if (!t.date) return false;
